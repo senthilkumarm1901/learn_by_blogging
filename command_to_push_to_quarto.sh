0b43d1f3eeac1f661f067cf69e35cc15c9976505
Senthil Kumar <senthilkumar.m1901@gmail.com>
2025-12-07 13:28:53 +0000

    Dec 7 - changes to index.qmd to allow search indexing

--- command_to_push_to_quarto.sh
+++ command_to_push_to_quarto.sh
@@ -1,1 +1,5 @@
-quarto publish quarto-pub
+- make changes in main branch from Github Codespaces and --> `git add`, `git commit ...`,tree git push origin main
+
+In GitHub CodeSpaces:
+- pip install quarto-cli
+- quarto publish gh-pages
